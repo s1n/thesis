38df5776ae39bc7168892ce941407dedb1692045
Jason Switzer <jswitzer@gmail.com>
2009-10-07 07:05:54 -0500

    [experiment] snagged the example for the AJAX API module

---
+++ google/exp/ajax.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+my $key = "";
+my $referer = "http://s1n.dyndns.org";
+my $search = Google::Search->Web(q => 'Jason Switzer',
+                                 key => $key,
+                                 referer => $referer);
+my $result = $search->first;
+while($result) {
+   say $result->number, " ", $result->uri;
+   $result = $result->next;\
+}
