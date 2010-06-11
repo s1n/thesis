5e89a89baaca64925046fa893f8cf97d6ca35a6e
Jason Switzer <jswitzer@gmail.com>
2010-06-11 08:20:01 -0500

    [exp] Old experimental stuff from previous advisor.

--- experiment/robot/robot.pl
+++ experiment/robot/robot.pl
@@ -5,25 +5,29 @@ use WWW::Robot;
 
 my $robot = new WWW::Robot('NAME' => 'TestRobot',
                            'VERSION' => '0.00001',
+                           'DELAY' => 1/60,
+                           'VERBOSE' => 1,
+                           'TRAVERSAL' => 'breadth',
                            'EMAIL' => 'test@example.com');
 
 $robot->addHook('follow-url-test', \&follow_test);
 $robot->addHook('invoke-on-contents', \&validate_contents);
 
-my $docroot = 'http://www.wikipedia.org';
+my $docroot = 'http://en.wikipedia.org';
 $robot->run($docroot);
 
 sub follow_test {
    my ($robot, $hook, $url) = @_;
-   say $url;
+   say "SPIDER => " . $url;
    return 0 unless $url->scheme eq 'http';
    return 0 if $url =~ /\.(gif|jpg|png|xbm|au|wav|mpg)$/;
+   return 1 if $url =~ /^wiki\//;
    return $url =~ /^$docroot/;
 }
 
 sub validate_contents {
    my ($robot, $hook, $url, $response, $structure) = @_;
-   print $response->content_type;
+   say $url . " => " . $response->content_type;
    return unless $response->content_type eq 'text/html';
 }
 
