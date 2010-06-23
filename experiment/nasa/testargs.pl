c8a96bda2d16487dbb6d6dfd4e6925d476f0c9d7
s1n <jswitzer@gmail.com>
2010-06-23 09:44:41 -0500

    [exp] Moose application test of commandline args.

---
+++ experiment/nasa/testargs.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+package SeedApp;
+use Moose;
+        
+with 'MooseX::Getopt';
+
+has 'dict' => (
+   metaclass => 'MooseX::Getopt::Meta::Attribute',
+   is => 'ro',
+   isa => 'Str',
+   default => '/usr/share/dict/words',
+   cmd_flag => 'dict',
+   cmd_aliases => 'd',
+);
+
+has 'affix' => (
+   metaclass => 'MooseX::Getopt::Meta::Attribute',
+   is => 'ro',
+   isa => 'Str',
+   default => 'affix.dat',
+   cmd_flag => 'affix',
+   cmd_aliases => 'a',
+);
+
+has 'matchlog' => (
+   metaclass => 'MooseX::Getopt::Meta::Attribute',
+   is => 'ro',
+   isa => 'Str',
+   default => 'matches.log',
+   cmd_flag => 'matchlog',
+   cmd_aliases => 'm',
+);
+
+1;
+
+use Modern::Perl;
+
+my $seed = SeedApp->new_with_options;
+say "dict = ", $seed->dict;
+say "affix = ", $seed->affix;
+say "matchlog = ", $seed->matchlog;
