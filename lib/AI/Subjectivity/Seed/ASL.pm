2928b7d9a76c7250ed8c7b51d58bb51050973bde
s1n <jswitzer@gmail.com>
2010-07-31 08:46:30 -0500

    [seed] Moves the logging out.

--- lib/AI/Subjectivity/Seed/ASL.pm
+++ lib/AI/Subjectivity/Seed/ASL.pm
@@ -1,4 +1,5 @@
 package AI::Subjectivity::Seed::ASL;
+
 use Modern::Perl;
 use Moose;
 
@@ -10,9 +11,6 @@ sub build {
    my $dictref = $self->dictionary;
    my $patref = $self->patterns;
 
-   open(LOG, '>' . $self->args->matchlog) or
-      die "Unable to create match log: $!\n";
-
    #loop over every word in the dictionary
    while(my ($key, $sign) = each(%$dictref)) {
       #first check if it's a positive word
@@ -23,7 +21,7 @@ sub build {
             #now check to see if it's a negative match
             if(exists($dictref->{$foo})) {
                #say "positive match of $key to $pospat with $1 finding $foo";
-               say LOG "$key, $foo";
+               #say LOG "$key, $foo";
                $dictref->{$foo}--;
                $dictref->{$key}++;
             } else {
@@ -32,7 +30,6 @@ sub build {
          }
       }
    }
-   close LOG;
 }
 
 no Moose;
