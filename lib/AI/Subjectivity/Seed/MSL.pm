8419486345498521daee32b4dc6e4695f24b157e
s1n <jswitzer@gmail.com>
2010-08-07 18:00:09 -0500

    [moby] Fixes the massive memory leak that was caused by splitting a line and returning the array. Now we loop through the thesaurus words and mark the synset rather than starting at the dictionary.

--- lib/AI/Subjectivity/Seed/MSL.pm
+++ lib/AI/Subjectivity/Seed/MSL.pm
@@ -3,6 +3,7 @@ package AI::Subjectivity::Seed::MSL;
 use Modern::Perl;
 use Text::Thesaurus::Moby;
 use Moose;
+use Devel::Cycle;
 
 extends 'AI::Subjectivity::Seed';
 with 'AI::Subjectivity::Seeder';
@@ -14,7 +15,6 @@ has 'mobyobj' => (
    default => sub { Text::Thesaurus::Moby->new }
 );
 
-#FIXME none of this works yet
 sub build {
    my $self = shift;
    my $dictref = $self->dictionary;
@@ -23,44 +23,25 @@ sub build {
    #load the thesaurus
    $self->mobyobj->load($self->args->thes);
 
-   #loop over every word in the dictionary
-   while(my ($word, $score) = each(%$dictref)) {
-      my @relatedwords;
-      $self->_trace_word($word, \@relatedwords);
-      for my $w(@relatedwords) {
-         my $delta = ($score == abs($score)) ? 1 : -1;
-         say "adjusting score $w by $delta" if $delta < 0;
-         my ($trimmed, undef, undef) = split /\#/, $w;
-         $dictref->{$trimmed} += $delta;
+   for my $line(@{$self->mobyobj->rawdata}) {
+      chomp $line;
+      my @words = split /,/, $line;
+      my $root = shift @words;
+      @words = undef && next if !$root;
+      chomp @words;
+      #say "rescoring root: $root";
+      for my $w(@words) {
+         next if !$w;
+         my $delta = ($dictref->{$root} && 
+                      $dictref->{$root} == abs($dictref->{$root})) ? 1 : -1;
+         #say "adjusting score $w by $delta";
+         if($w !~ /\S+/) {
+            say "JUNK: $line";
+         }
+         $dictref->{$root} += $delta;
       }
+      @words = undef;
    }
-}
-
-sub _trace_word {
-   my ($self, $root, $wordsref) = @_;
-   #say "tracing $root as $sense";
-
-   my @words = $self->_query_word($root);
-   for my $w(@words) {
-      my @newwords = $self->_query_word($w);
-      for my $nw(@newwords) {
-         #say "checking newword $nw";
-         push @$wordsref, $nw if !grep(/$nw/, @$wordsref);
-         #say "dup $nw" if grep(/$nw/, @$wordsref);
-      }
-   }
-}
-
-sub _query_word {
-   my ($self, $word) = @_;
-#FIXME integrate Text::Thesaurus::Moby
-   #my @results = $self->wordnet->queryWord($phrase, $word);
-   my $results = $self->mobyobj->synset($word);
-   #my $dump = join(", ", @results);
-   #say "$word => $dump" if $dump;
-   return @$results if $results;
-   my @junk;
-   return @junk;
 }
 
 no Moose;
