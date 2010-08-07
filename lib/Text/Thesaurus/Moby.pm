de3dd4b678bbc6f67f4d2ab24ee61b9baa382e8a
s1n <jswitzer@gmail.com>
2010-08-07 17:58:26 -0500

    [moby] Searching is very expensive as perl leaks arrays returned, even if undef. No cycles were found with Devel::Cycle. By using rawdata directly, the user can make a single leak-free pass over the file. The result is 80% less memory usage.

--- lib/Text/Thesaurus/Moby.pm
+++ lib/Text/Thesaurus/Moby.pm
@@ -2,9 +2,9 @@ package Text::Thesaurus::Moby;
 
 use warnings;
 use strict;
-use Data::Dumper;
 use Carp;
-use Text::CSV;
+use Fcntl qw/O_RDONLY/;
+use Tie::File;
 
 sub new {
    my $class = shift;
@@ -19,33 +19,38 @@ sub new {
 
 sub load {
    my ($self, $thes) = @_;
-   my $csv = Text::CSV->new();
-   open THES, $thes or croak "Unable to open thesaurus: $!"; 
-   while(defined (my $line = <THES>)) {
-      if($csv->parse($line)) {
-         my @columns = $csv->fields();
-         my $rootw = shift @columns;
-         push @{$self->{thes}->{$rootw}}, @columns;
-      } else {
-         my $err = $csv->error_input;
-         croak "Failed to parse line: $err"
-      }
-   }
-   close THES;
-   #print Dumper($self->{thes}->{'freedom'});
+   $self->file($thes);
+   tie @{$self->rawdata}, 'Tie::File', $self->file, mode => O_RDONLY;
+}
+
+sub file {
+   my ($self, $file) = @_;
+   $self->{file} = $file if $file;
+   return $self->{file};
+}
+
+sub rawdata {
+   my ($self, $data) = @_;
+   $self->{raw} = $data if $data;
+   return $self->{raw};
 }
 
 sub synset {
    my ($self, $rootword) = @_;
-   #print $self->{thes}->{$rootword} . "\n";
-   return $self->{thes}->{$rootword};
+   for my $rw(@{$self->{raw}}) {
+      return $rw if $rw =~ /^$rootword,/;
+   }
+   return undef;
 }
 
 sub search {
    my ($self, $rootword) = @_;
    my @synset;
-   for my $rw(keys %{$self->{thes}}) {
-      push @synset, $rw if grep { $_ eq $rw } @{$self->{thes}->{$rw}};
+   for my $rw(@{$self->rawdata}) {
+      if($rw =~ /,$rootword,?/) {
+         my @words = split /,/, $rw;
+         push @synset, $words[0] if @words;
+      }
    }
    return @synset;
 }
