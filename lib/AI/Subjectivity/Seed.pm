8ffc8865ad713bffe4c2057ef73cab49055b7fe0
s1n <jswitzer@gmail.com>
2010-09-13 23:44:56 -0500

    [docs] More documentation about the Seed objects.

--- lib/AI/Subjectivity/Seed.pm
+++ lib/AI/Subjectivity/Seed.pm
@@ -102,3 +102,98 @@ sub signed {
 
 no Moose;
 1;
+
+=pod
+
+=head1 NAME
+
+AI::Subjectivity::Seed - base class for all seeding algorithms.
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+All seeding algorithms should follow the same interaction as they share many
+things in common. No matter the algorithm, the destination lexicon file will
+be of the same format, so the B<load> and B<save> will be common. Also, this
+means that lexicons built from two different algorithms can be loaded into
+this class and compared directly with the B<accuracy> method.
+
+By extending this class, subclasses can access its lexicon by the I<lexicon>
+attribute.
+
+A potential point that will need refactoring is the trace attribute. This is
+something that all algorithms should have in common but wind up implementing
+uniquely to the problem they represent. 
+
+=head1 ATTRIBUTES
+
+=head2 lexicon(data)
+
+Contains the current lexicon data. This is stored as a hash:
+
+ word => lex_score
+
+where I<word> is a unique lexicon word and I<lex_score> is the lexicon score
+for the I<word>. The higher the score, the more positive the word; conversely,
+the lower the score, the more negative the word.
+
+If the I<data> parameter is not specified, this is an accessor, otherwise a
+modifier. B<It is not recommended to use this method as a modifier.>
+
+=head2 trace(word)
+
+Single word to trace. This word may appear anywhere found by the algorithm used.
+If I<word> is not specified, this is an accessor, otherwise a modifier.
+
+=head1 METHODS
+
+=head2 new
+
+Constructs a new instance. For this class, this is a generic object that is
+incapable of building a seeded lexicon.
+
+=head2 load
+
+Loads a lexicon file into memory. Afterwards, the lexicon can be accessed via
+the B<lexicon> attribute.
+
+=head2 save
+
+Saves the current lexicon data to a file.
+
+=head2 accuracy
+
+Compares two lexicons and returns a L<Stats::Measure>, which can report various
+accuracy measures.
+
+=head2 signed
+
+Returns -1 for negative values, 1 for positive values, and 0 otherwise.
+
+=head1 LEXICON
+
+Lexicons are stored with 1 word and subjectivity score per line. The line has
+the following format:
+
+ word_1,score_1
+ ...
+ word_n,score_n
+
+where I<word_n> is the nth word and I<score_n> is the score for the same (nth)
+word. The score should be either positive or negative, but indeterminate scores
+may also be present.
+
+=head1 AUTHOR
+
+Jason Switzer <s1n@voidreturn.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jason Switzer. The entire module may be
+redistributed and/or modified under the terms of the MIT license. See
+L<http://www.opensource.org/licenses/mit-license.php>.
+
+=cut
+
+__END__
--- lib/AI/Subjectivity/Seeder.pm
+++ lib/AI/Subjectivity/Seeder.pm
@@ -4,3 +4,51 @@ use Moose::Role;
 after 'build' => sub { };
 
 1;
+
+=pod
+
+=head1 NAME
+
+AI::Subjectivity::Seeder - role that all seeding algorithms must abide.
+
+=head1 SYNOPSIS
+
+ #build a seeder called Foo
+ package AI::Subjectivity::Seed::Foo;
+
+ use Moose;
+
+ extends 'AI::Subjectivity::Seed';
+ with 'AI::Subjectivity::Seeder';
+
+ #two parameters: self and a word to trace (if enabled)
+ sub build {
+    my ($self, $trace) = @_;
+    #...
+ }
+ 1;
+
+=head1 DESCRIPTION
+
+This provides a contract all seeding algorithms must abide. In order to
+seed a subjectivity lexicon, the only method a package must implement is
+the B<build> method. B<build> takes 2 parameters: I<self>, and a I<tracer>.
+
+The I<tracer> parameter is a word to trace. Each seeding algorithm may determine
+how and what is traced, but this is typically requested from the caller. For
+example, if we wanted to trace what happens to the word 'used', pass that in
+as the I<tracer> parameter.
+
+=head1 AUTHOR
+
+Jason Switzer <s1n@voidreturn.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jason Switzer. The entire module may be
+redistributed and/or modified under the terms of the MIT license. See
+L<http://www.opensource.org/licenses/mit-license.php>.
+
+=cut
+
+__END__
