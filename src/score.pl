ef030cff5b1f17ce1ea7f8cc47a5992181e5c045
s1n <jswitzer@gmail.com>
2010-09-25 19:22:21 -0500

    [score] Renames the arguments to --reference and --test.

--- src/score.pl
+++ src/score.pl
@@ -6,23 +6,23 @@ use Moose;
         
 with 'MooseX::Getopt';
 
-has 'base' => (
+has 'reference' => (
    metaclass => 'MooseX::Getopt::Meta::Attribute',
    is => 'ro',
    isa => 'Str',
    default => 'lexicon.dat',
-   documentation => 'Base lexicon considered the reference lexicon.',
-   cmd_flag => 'base',
-   cmd_aliases => 'b',
+   documentation => 'Reference lexicon considered the authority lexicon.',
+   cmd_flag => 'reference',
+   cmd_aliases => 'r',
 );
 
-has 'other' => (
+has 'test' => (
    metaclass => 'MooseX::Getopt::Meta::Attribute',
    is => 'ro',
    isa => 'Str',
-   documentation => 'Secondary lexicon to compare against the base.',
+   documentation => 'Test lexicon to compare against the reference.',
    default => 'lexicon.dat',
-   cmd_flag => 'other',
+   cmd_flag => 'test',
    cmd_aliases => 't',
 );
 
@@ -33,11 +33,11 @@ use Data::Dumper;
 use AI::Subjectivity::Seed;
 
 my $arguments = ScoreArgs->new_with_options;
-my $base = AI::Subjectivity::Seed->new;
-my $other = AI::Subjectivity::Seed->new;
-$base->load($arguments->base);
-$other->load($arguments->other);
-my $measure = $base->accuracy($other);
+my $ref = AI::Subjectivity::Seed->new;
+my $test = AI::Subjectivity::Seed->new;
+$ref->load($arguments->reference);
+$test->load($arguments->test);
+my $measure = $ref->accuracy($test);
 say "   Accuracy: ", $measure->accuracy;
 say "  Precision: ", $measure->precision;
 say "     Recall: ", $measure->recall;
@@ -60,14 +60,14 @@ score.pl - scores the accuracy between two lexicons.
 =head1 SYNOPSIS
 
  #compare the current lexicon against the GI lexicon
- ./score.pl --base ../results/gi.dat --other lexicon.dat
+ ./score.pl --reference ../results/gi.dat --test lexicon.dat
 
 =head1 DESCRIPTION
 
 This script is used to check the current accuracy against a known lexicon.
-The term 'base' lexicon means that this lexicon is considered the reference 
-lexicon. The 'other' lexicon is any other lexicon that the user wishes to check
-its accuracy against the base lexicon.
+The term 'reference' lexicon means that this lexicon is considered the reference
+lexicon. The 'test' lexicon is any other lexicon that the user wishes to check
+its accuracy against the reference lexicon.
 
 Generally, the user will do this during development or training of the lexicon.
 For example, to know if a new algorithm is working well, the user may wish to
@@ -77,6 +77,17 @@ The GI lexicon can be found at
 L<http://www.wjh.harvard.edu/~inquirer/homecat.htm>. This comparison will report
 all metrics that can be computed by L<Stats::Measure> as indicated here:
 L<http://github.com/s1n/thesis/blob/master/lib/Stats/Measure.pm>.
+
+=head1 OPTIONS
+
+=head2 --reference|r
+
+Sets the reference lexicon, which will be the authority.
+
+=head2 --test|t
+
+Sets the testing lexicon. Only words found in the reference lexicon will be
+checked. The rest will be marked as 'unknown'.
 
 =cut
 
