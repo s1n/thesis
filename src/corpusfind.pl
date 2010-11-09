14f307997f16df038b8bb95ac4d8cf0bbdee7c9e
s1n <jswitzer@gmail.com>
2010-11-09 14:44:35 -0600

    [nasa] Improves corpusfind.pl to give better reporting as to the number of lines processed, the number of times lexicon phrases are spotted, and the number of unique lexicon words found.

--- src/corpusfind.pl
+++ src/corpusfind.pl
@@ -52,26 +52,49 @@ has 'verbose' => (
 use Data::Dumper;
 use AI::Subjectivity::Seed;
 use Text::Corpus::NASA;
+use Term::ProgressBar;
 
 my $arguments = CorpusArgs->new_with_options;
 my $ref = AI::Subjectivity::Seed->new;
 my $corpus = Text::Corpus::NASA->new({file => $arguments->corpus});
 $ref->load($arguments->lexicon);
 
+my $wordsfound = 0;
+my %wordsfound;
+my $sentancesfound = 0;
+my $ccount = `wc -l $arguments->{corpus}`;
+chomp $ccount;
+my $pb = Term::ProgressBar->new({count => $ccount,
+                                 name => "Searching",
+                                 ETA => 'linear'});
 while(my $cline = $corpus->nextline) {
+   my $prevwfound = scalar keys %wordsfound;
+   say "Checking line $sentancesfound...";
    for my $cw(keys %{$ref->lexicon}) {
-      say "Checking for $cw in corpus..." if $arguments->verbose;
+      #say "Checking for $cw in corpus..." if $arguments->verbose;
       if(($arguments->matches && $cline =~ /\W$cw\W/) ||
          (!$arguments->matches && $cline !~ /\W$cw\W/)) {
-         print "word=$cw";
+         $wordsfound{$cw}++;
          if($arguments->verbose) {
+            print "word=$cw";
             print ", score=", $ref->lexicon->{$cw}->{score};
             print ", weight=", $ref->lexicon->{$cw}->{weight};
+            print "\n";
          }
-         print "\n";
       }
    }
+   $sentancesfound ++ if (scalar keys %wordsfound) > $prevwfound;
+   $pb->update($sentancesfound);
 }
+$pb->update($ccount);
+
+my $count = 0;
+while(my ($w, $c) = each %wordsfound) {
+   $count += $c;
+}
+say "Total Word Matches:", $count;
+say "Unique Word Matches:", scalar keys %wordsfound;
+say "Total Sentance Matches:", $sentancesfound;
 
 =pod
 
