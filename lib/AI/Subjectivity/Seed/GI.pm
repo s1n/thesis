9bcc2c189ceeb625350d73cb3fa6b21f6da02ad7
s1n <jswitzer@gmail.com>
2010-11-10 10:33:20 -0600

    [seed] Fixes the function name used for stripwords.

--- lib/AI/Subjectivity/Seed/GI.pm
+++ lib/AI/Subjectivity/Seed/GI.pm
@@ -37,7 +37,7 @@ sub build_tags {
       my $text = $link->text;
       my @tokens = split /\#/, $text;
       $text = lc shift @tokens;
-      next if !$text || $self->is_stripwords($text);
+      next if !$text || $self->is_stripword($text);
       $lexref->{$text}->{score} += $delta;
 
       if($text eq $trace) {
