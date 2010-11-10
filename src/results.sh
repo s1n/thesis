c9f68b435be4935e971c879ff2333e806071551e
s1n <jswitzer@gmail.com>
2010-11-10 14:55:30 -0600

    [results] Fixes to perform boosting (skipped currently). Also passes the --lexicon option manually and sets the --stripwords option to all tests.

--- src/results.sh
+++ src/results.sh
@@ -2,10 +2,35 @@
 
 function score() {
    local TEST="$1"
-   echo "Performance against GI:"
-   perl -Ilib src/score.pl --reference results/gi.dat --test "results/$TEST.dat"
+   local REFLEX="results/gi.dat"
+   #local REFLEX="$2" || "results/gi.dat"
+   echo "Performance against GI ($REFLEX):"
+   perl -Ilib src/score.pl --reference "$REFLEX" --test "results/$TEST.dat"
+   #perl -Ilib src/score.pl --reference results/gi.dat --test "results/$TEST.dat"
    echo "Performance against SSL:"
    perl -Ilib src/score.pl --reference results/manual.dat --test "results/$TEST.dat"
+}
+
+function runtest() {
+   local TEST="$1"
+   local EXTRAARGS="$2"
+   echo ""
+   echo "Running test: $TEST $EXTRAARGS to $TEST.dat"
+   perl -Ilib src/seed.pl --configfile "conf/$TEST.json" --lexicon "results/$TEST.dat" --stripwords data/stripwords.txt "$EXTRAARGS" >& "log/$TEST.log"
+   score $TEST $EXTRAARGS
+}
+
+function runboosting() {
+   local TEST="$1"
+   for algo in ada inv; do
+      for iter in 3 10; do
+         BOOSTTEST=$TEST"_"$algo$iter
+         #echo "TEST=$NEWTEST"
+         BOOSTALGO=`echo "$algo" | { dd bs=1 count=1 conv=ucase 2>/dev/null; cat; }`
+         runtest "$BOOSTTEST", "--boost-iter $iter --lexicon $BOOSTTEST.dat --boost $BOOSTALGO"Boost" --boost-ref results/manual.dat"
+         score $BOOSTTEST "results/boost_test.dat"
+      done;
+   done
 }
 
 mkdir log
@@ -22,8 +47,8 @@ for f in `ls conf/*.json`; do
       score "$TEST"
       continue
    fi
-   echo ""
-   echo "Running test: $TEST"
-   perl -Ilib src/seed.pl --configfile "$f" >& "log/$TEST.log"
-   score "$TEST"
+
+   runtest "$TEST"
+   #runboosting "$TEST"
+
 done
